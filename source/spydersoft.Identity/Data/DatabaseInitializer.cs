e3a851e8bd4bd16110dbe7313b70b42844bbd274
Matt Gerega <gerega@gmail.com>
2019-03-17 22:05:05 -0400

    Added an admin user to the seed data for new DBs

--- source/spydersoft.Identity/Data/DatabaseInitializer.cs
+++ source/spydersoft.Identity/Data/DatabaseInitializer.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using IdentityServer4;
 using IdentityServer4.EntityFramework.DbContexts;
 using IdentityServer4.EntityFramework.Mappers;
@@ -9,12 +6,21 @@ using IdentityServer4.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using spydersoft.Identity.Models.Identity;
 
 namespace spydersoft.Identity.Data
 {
     public class DatabaseInitializer
     {
         private readonly IApplicationBuilder _app;
+        private ILogger _log;
 
         public DatabaseInitializer(IApplicationBuilder app)
         {
@@ -25,12 +31,61 @@ namespace spydersoft.Identity.Data
         {
             using (var serviceScope = _app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
             {
+                _log = serviceScope.ServiceProvider.GetRequiredService<ILogger<DatabaseInitializer>>();
+
                 PerformDatabaseMigrations(serviceScope);
                 SeedDatabase(serviceScope);
             }
         }
 
+        public void InitializeAutomapper()
+        {
+            _log.LogDebug("Initializing Automapper");
+            AutoMapper.Initialize();
+        }
+
+        #region Database Migration Methods
+
+        private void PerformDatabaseMigrations(IServiceScope serviceScope)
+        {
+            Task persistedGrantTask =
+                DoMigrationIfNeeded(serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>(), "ASP Net Grants Database");
+
+            Task.WaitAll(persistedGrantTask);
+
+            Task appDbTask =
+                DoMigrationIfNeeded(serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>(), "ASP Net User Database ");
+
+            Task.WaitAll(appDbTask);
+
+            Task configTask =
+                DoMigrationIfNeeded(serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>(), "Identity Server 4 Configuration Database");
+
+            Task.WaitAll(configTask);
+        }
+
+        private async Task DoMigrationIfNeeded(DbContext context, string databaseName)
+        {
+            _log.LogDebug("Checking {0} for pending migrations.", databaseName);
+            bool hasMigrations = (await context.Database.GetPendingMigrationsAsync()).Any();
+            if (hasMigrations)
+            {
+                _log.LogInformation("Migrating {0}.", databaseName);
+                await context.Database.MigrateAsync();
+            }
+        }
+
+        #endregion Database Migration Methods
+
+        #region Seeding Functions
+
         private void SeedDatabase(IServiceScope serviceScope)
+        {
+            SeedIdentityServerConfigurationDatabase(serviceScope);
+            SeedAspNetIdentityDatabase(serviceScope);
+        }
+
+        private void SeedIdentityServerConfigurationDatabase(IServiceScope serviceScope)
         {
             ConfigurationDbContext context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
 
@@ -40,6 +95,7 @@ namespace spydersoft.Identity.Data
                 {
                     context.Clients.Add(client.ToEntity());
                 }
+
                 context.SaveChanges();
             }
 
@@ -49,6 +105,7 @@ namespace spydersoft.Identity.Data
                 {
                     context.IdentityResources.Add(resource.ToEntity());
                 }
+
                 context.SaveChanges();
             }
 
@@ -58,34 +115,81 @@ namespace spydersoft.Identity.Data
                 {
                     context.ApiResources.Add(resource.ToEntity());
                 }
+
                 context.SaveChanges();
             }
         }
 
-        private void PerformDatabaseMigrations(IServiceScope serviceScope)
+        private void SeedAspNetIdentityDatabase(IServiceScope serviceScope)
         {
-            Task appDbTask =
-                DoMigrationIfNeeded(serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>());
-
-            Task persistedGrantTask =
-                DoMigrationIfNeeded(serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>());
-
-            Task configTask =
-                DoMigrationIfNeeded(serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>());
-
-
-            Task.WaitAll(appDbTask, persistedGrantTask, configTask);
-        }
-
-        private async Task DoMigrationIfNeeded(DbContext context)
-        {
-            bool hasMigrations = (await context.Database.GetPendingMigrationsAsync()).Any();
-            if (hasMigrations)
+            var roleMgr = serviceScope.ServiceProvider.GetRequiredService<RoleManager<ApplicationRole>>();
+            var adminRole = roleMgr.FindByNameAsync("admin").Result;
+            if (adminRole == null)
             {
-                await context.Database.MigrateAsync();
+                IdentityResult result = roleMgr.CreateAsync(new ApplicationRole()
+                {
+                    Name = "admin"
+                }).Result;
+
+                if (!result.Succeeded)
+                {
+                    _log.LogError("Unable to create admin role : {0}", result.ToString());
+                }
             }
+
+
+            var userMgr = serviceScope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+            var alice = userMgr.FindByNameAsync("admin").Result;
+            if (alice == null)
+            {
+                alice = new ApplicationUser
+                {
+                    UserName = "admin"
+                };
+                var result = userMgr.CreateAsync(alice, "Ch@ng3m3").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                alice = userMgr.FindByNameAsync("admin").Result;
+
+                result = userMgr.AddClaimsAsync(alice, new Claim[]{
+                                new Claim(JwtClaimTypes.Name, "System Administrator"),
+                                new Claim(JwtClaimTypes.GivenName, "System"),
+                                new Claim(JwtClaimTypes.FamilyName, "administrator"),
+                                new Claim(JwtClaimTypes.Email, "admin@localhost.net"),
+                                new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                                new Claim(JwtClaimTypes.WebSite, "123 NoWhere"),
+                                new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 69118, 'country': 'Germany' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                            }).Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                result = userMgr.AddToRoleAsync(alice, "admin").Result;
+                if (!result.Succeeded)
+                {
+                    throw new Exception(result.Errors.First().Description);
+                }
+
+                _log.LogInformation("admin created");
+            }
+            else
+            {
+                _log.LogDebug("admin already exists");
+            }
+      
         }
-        // scopes define the resources in your system
+
+        #endregion Seeding Functions
+
+
+
+
+        #region Identity Server Configuration Object Creators
+
         private IEnumerable<IdentityResource> GetIdentityResources()
         {
             return new List<IdentityResource>
@@ -162,9 +266,6 @@ namespace spydersoft.Identity.Data
             };
         }
 
-        public void InitializeAutomapper()
-        {
-            Data.AutoMapper.Initialize();
-        }
+        #endregion Identity Server Configuration Object Creators
     }
 }
--- source/spydersoft.Identity/Models/AccountViewModels/LoginViewModel.cs
+++ source/spydersoft.Identity/Models/AccountViewModels/LoginViewModel.cs
@@ -5,7 +5,7 @@ namespace spydersoft.Identity.Models.AccountViewModels
     public class LoginViewModel
     {
         [Required]
-        [StringLength(256, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [StringLength(256, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 2)]
         public string UserName { get; set; }
 
         [Required]
