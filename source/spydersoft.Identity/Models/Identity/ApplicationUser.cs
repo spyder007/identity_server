d6b168fcd27b693e04fb7a6e3e2bace19eb426c7
Matt Gerega <gerega@gmail.com>
2021-10-14 13:12:09 -0400

    Fixed build errors for device code consent page

--- source/spydersoft.Identity/Models/Identity/ApplicationUser.cs
+++ source/spydersoft.Identity/Models/Identity/ApplicationUser.cs
@@ -5,6 +5,7 @@ namespace spydersoft.Identity.Models.Identity
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData]
         public string Name { get; set; }
     }
 }
