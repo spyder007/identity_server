bb1c0f883c31d216230339030ec04fdaa90688c9
Matt Gerega <gerega@gmail.com>
2023-06-22 16:20:30 -0400

    Upgraded Duende to 2.3.2

--- source/spydersoft.Identity/Data/Migrations/ConfigurationDb/20230622201900_Duende_2_3_0.Designer.cs
+++ source/spydersoft.Identity/Data/Migrations/ConfigurationDb/20230622201900_Duende_2_3_0.Designer.cs
@@ -4,6 +4,7 @@ using Duende.IdentityServer.EntityFramework.DbContexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace spydersoft.Identity.Data.Migrations.ConfigurationDb
 {
     [DbContext(typeof(ConfigurationDbContext))]
-    partial class ConfigurationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230622201900_Duende_2_3_0")]
+    partial class Duende_2_3_0
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
