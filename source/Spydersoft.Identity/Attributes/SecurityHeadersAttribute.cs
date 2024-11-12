d16d1cc39f5c8583a41c6e2dc5e4c074e68dc693
Matt Gerega <gerega@gmail.com>
2024-11-12 14:46:38 -0500

    Migrate to platform functions

--- source/Spydersoft.Identity/Attributes/SecurityHeadersAttribute.cs
+++ source/Spydersoft.Identity/Attributes/SecurityHeadersAttribute.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
 
+using System;
+
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
@@ -13,6 +15,7 @@ namespace Spydersoft.Identity.Attributes
     /// Implements the <see cref="ActionFilterAttribute" />
     /// </summary>
     /// <seealso cref="ActionFilterAttribute" />
+    [AttributeUsage(AttributeTargets.Class)]
     public class SecurityHeadersAttribute : ActionFilterAttribute
     {
         /// <summary>
--- source/Spydersoft.Identity/Program.cs
+++ source/Spydersoft.Identity/Program.cs
@@ -1,11 +1,29 @@
 ﻿using System;
+using System.Reflection;
+
+using Duende.IdentityServer;
 
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+using OpenTelemetry.Trace;
 
 using Serilog;
 
-using Spydersoft.Identity;
+using Spydersoft.Identity.Data;
+using Spydersoft.Identity.Extensions;
+using Spydersoft.Identity.Models.Identity;
+using Spydersoft.Identity.Options;
+using Spydersoft.Identity.Services;
+using Spydersoft.Platform.Hosting.Options;
+using Spydersoft.Platform.Hosting.StartupExtensions;
 
 IConfigurationRoot baseConfig = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
@@ -20,15 +38,119 @@ try
     Log.Information("identityServer starting.");
     WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-    _ = builder.Host.UseSerilog((context, services, configuration) => _ = configuration.ReadFrom.Configuration(context.Configuration));
+    builder.AddSpydersoftTelemetry(typeof(Program).Assembly,
+        additionalTraceConfiguration: (telemetry) => telemetry
+            .AddSource(IdentityServerConstants.Tracing.Basic)
+            .AddSource(IdentityServerConstants.Tracing.Cache)
+            .AddSource(IdentityServerConstants.Tracing.Services)
+            .AddSource(IdentityServerConstants.Tracing.Stores)
+            .AddSource(IdentityServerConstants.Tracing.Validation)
+            .AddSqlClientInstrumentation(),
+        additionalMetricsConfiguration: null,
+        additionalLogConfiguration: null)
+        .AddSpydersoftSerilog();
 
-    var startup = new Startup(builder.Configuration);
-    startup.ConfigureServices(builder.Services);
+    AppHealthCheckOptions healthCheckOptions = builder.AddSpydersoftHealthChecks();
+
+    var connString = builder.Configuration.GetConnectionString("IdentityConnection");
+    var cacheConnection = builder.Configuration.GetConnectionString("RedisCache");
+    var migrationsAssembly = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
+
+    _ = builder.Services.Configure<SendgridOptions>(builder.Configuration.GetSection(SendgridOptions.Name));
+    _ = builder.Services.Configure<ConsentOptions>(builder.Configuration.GetSection(ConsentOptions.SettingsKey));
+
+    _ = builder.Services.ConfigureNonBreakingSameSiteCookies();
+    _ = builder.Services.AddHttpContextAccessor();
+    // Add framework builder.Services.
+    _ = builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connString));
+
+    _ = builder.Services.AddDbContext<DataProtectionDbContext>(options => options.UseSqlServer(connString));
+    _ = builder.Services.AddDataProtection()
+        .SetApplicationName("identity-server")
+        .PersistKeysToDbContext<DataProtectionDbContext>();
+
+    if (!string.IsNullOrEmpty(cacheConnection))
+    {
+        _ = builder.Services.AddStackExchangeRedisCache(options => options.Configuration = cacheConnection);
+    }
+
+    _ = builder.Services.AddAutoMapper(typeof(Program));
+    _ = builder.Services.AddIdentity<ApplicationUser, ApplicationRole>(options => options.User.RequireUniqueEmail = true)
+        .AddEntityFrameworkStores<ApplicationDbContext>()
+        .AddDefaultTokenProviders();
+
+    // Add application builder.Services.
+    _ = builder.Services.AddTransient<IEmailSender, EmailSender>();
+
+    _ = builder.Services.AddMvc(options =>
+    {
+        options.SuppressAsyncSuffixInActionNames = false;
+        options.EnableEndpointRouting = false;
+    });
+
+    // this adds the Configuration Store (clients, resources) and then
+    // the Operation Store (codes, tokens, consents)
+    _ = builder.Services.AddIdentityServer()
+        .AddAspNetIdentity<ApplicationUser>()
+        .AddConfigurationStore(options => options.ConfigureDbContext = builder =>
+                builder.UseSqlServer(connString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly)))
+        .AddOperationalStore(options =>
+        {
+            options.ConfigureDbContext = builder =>
+                builder.UseSqlServer(connString,
+                    sql => sql.MigrationsAssembly(migrationsAssembly));
+
+            // this enables automatic token cleanup. this is optional.
+            options.EnableTokenCleanup = true;
+            options.TokenCleanupInterval = 30;
+        });
+
+    var providerSettings = new ProviderOptions();
+    builder.Configuration.GetSection(ProviderOptions.SettingsKey).Bind(providerSettings);
+    _ = builder.Services.AddAuthentication()
+        .AddGoogle(option =>
+        {
+            option.SignInScheme = IdentityServerConstants.ExternalCookieAuthenticationScheme;
+            option.ClientId = providerSettings.GoogleClientId;
+            option.ClientSecret = providerSettings.GoogleClientSecret;
+        });
+    _ = builder.Services.AddAuthorization();
+    _ = builder.Services.AddHealthChecks()
+        .AddSqlServer(connString, null, null, "sqlserver", null, ["ready"], null);
 
     WebApplication app = builder.Build();
-    startup.Configure(app, app.Environment);
+    // this will do the initial DB population, but we only need to do it once
+    // this is just in here as a easy, yet hacky, way to get our DB created/populated
+    var dbInitialize = new DatabaseInitializer(app);
+    dbInitialize.InitializeDatabase();
 
-    app.Run();
+    _ = builder.Environment.IsDevelopment() ? app.UseDeveloperExceptionPage() : app.UseExceptionHandler("/Home/Error");
+
+    _ = app.UseSpydersoftHealthChecks(healthCheckOptions)
+            .UseCookiePolicy()
+            .UseStaticFiles();
+    var forwardedHeadersOptions = new ForwardedHeadersOptions
+    {
+        ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+    };
+    forwardedHeadersOptions.KnownNetworks.Clear();
+    forwardedHeadersOptions.KnownProxies.Clear();
+
+    _ = app.UseForwardedHeaders(forwardedHeadersOptions)
+        .UseOpenTelemetryPrometheusScrapingEndpoint()
+        .UseAuthentication()
+        .UseRouting()
+        .UseIdentityServer()
+        .UseAuthorization()
+        .UseEndpoints(endpoints => _ = endpoints.MapControllers());
+
+    _ = app.UseMvc(routes => _ = routes.MapRoute(
+            name: "default",
+            template: "{controller}/{action}/{id?}",
+            defaults: new { controller = "Home", action = "Index" }));
+
+    await app.RunAsync();
 }
 catch (Exception ex)
 {
@@ -37,5 +159,5 @@ catch (Exception ex)
 finally
 {
     Log.Information("identityServer shut down complete");
-    Log.CloseAndFlush();
+    await Log.CloseAndFlushAsync();
 }
